8d48bec1814f393eccbc7f86a6f8c4e17126834a
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-09-13 12:40:43 -0700

    safety

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,3 @@
-// Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
-// middleware functions from `auth-middleware.js`. You will need them here!
 const router = require('express').Router()
 const User = require('../users/users-model')
 const bcrypt = require('bcryptjs')
@@ -26,28 +24,6 @@ router.post(
     }
   },
 )
-/**
-  1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "user_id": 2,
-    "username": "sue"
-  }
-
-  response on username taken:
-  status 422
-  {
-    "message": "Username taken"
-  }
-
-  response on password three chars or less:
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
- */
 
 router.post(
   '/login',
@@ -74,21 +50,6 @@ router.post(
     }
   },
 )
-/**
-  2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "message": "Welcome sue!"
-  }
-
-  response on invalid credentials:
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
- */
 
 router.get('/logout', (req, res, next) => {
   if (req.session.user) {
@@ -107,21 +68,5 @@ router.get('/logout', (req, res, next) => {
     })
   }
 })
-/**
-  3 [GET] /api/auth/logout
 
-  response for logged-in users:
-  status 200
-  {
-    "message": "logged out"
-  }
-
-  response for not-logged-in users:
-  status 200
-  {
-    "message": "no session"
-  }
- */
-
-// Don't forget to add the router to the `exports` object so it can be required in other modules
 module.exports = router
