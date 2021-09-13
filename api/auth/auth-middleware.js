8193903e82e43b868cdd8ab868b06e5921189532
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-09-13 12:41:32 -0700

    cleaned middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,12 +1,5 @@
 const User = require('../users/users-model')
-/*
-  If the user does not have a session saved in the server
 
-  status 401
-  {
-    "message": "You shall not pass!"
-  }
-*/
 function restricted(req, res, next) {
   if (req.session.user) {
     next()
@@ -18,14 +11,6 @@ function restricted(req, res, next) {
   }
 }
 
-/*
-  If the username in req.body already exists in the database
-
-  status 422
-  {
-    "message": "Username taken"
-  }
-*/
 async function checkUsernameFree(req, res, next) {
   try {
     const { username } = req.body
@@ -44,14 +29,6 @@ async function checkUsernameFree(req, res, next) {
   }
 }
 
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
-*/
 async function checkUsernameExists(req, res, next) {
   const { username } = req.body
   try {
@@ -70,14 +47,6 @@ async function checkUsernameExists(req, res, next) {
   }
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
-*/
 function checkPasswordLength(req, res, next) {
   const { password } = req.body
 
@@ -91,7 +60,6 @@ function checkPasswordLength(req, res, next) {
   }
 }
 
-// Don't forget to add these to the `exports` object so they can be required in other modules
 module.exports = {
   restricted,
   checkUsernameFree,
